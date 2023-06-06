92bea67f53fef1ee8c8a7a479937426ba1982d44
YuHayeon <youha0930@gmail.com>
2023-06-06 10:39:24 +0900

    add: Ch05-05. Testcontainers를 이용한 통합 테스트 환경 구성

--- src/main/java/com/example/project/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/example/project/pharmacy/entity/Pharmacy.java
@@ -2,19 +2,22 @@ package com.example.project.pharmacy.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "pharmacy")
 public class Pharmacy {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String pharmacyName;
--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -1,16 +1,35 @@
 package com.example.project.pharmacy.repository
 
+import com.example.project.AbstractIntegrationContainerBaseTest
+import com.example.project.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import spock.lang.Specification
 
-@SpringBootTest
-class PharmacyRepositoryTest extends Specification {
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
-    private PharmacyRepository pharmacyRepository
+    PharmacyRepository pharmacyRepository
 
-    def "test"() {
-        pharmacyRepository.save()
+    def "PharmacyRepository save"() {
+        given:
+        String address = "서울시 강남구 도곡동"
+        String name = "즐거운 약국"
+        Double latitude = 36.11
+        Double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getPharmacyAddress() == address
+        result.getPharmacyName() == name
+        result.getLatitude() == latitude
+        result.getLongitude() == longitude
     }
 }
